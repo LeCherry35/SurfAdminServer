423ff38ea4d3ea745aba0b301bf8a7da00f725ba
Cherry <lenyamm35@gmail.com>
2023-10-29 00:33:43 +0300

    cors refresh

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const errorMiddleware = require('./middleware/error-middleware')
 const PORT = process.env.PORT || 5000
 const app = express()
 
+app.use(cors())
 app.use(cookieParser());
 app.use('/api', router)
 app.use(errorMiddleware)
